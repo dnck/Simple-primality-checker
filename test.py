0b3a4c4009dc8e2e8330ab2531fbf4faaf5f811a
Dan Cook <cook@Dans-MacBook-Pro.local>
2019-09-09 18:54:37 +0200

    add timing of primality checks in test.py

--- test.py
+++ test.py
@@ -2,6 +2,8 @@
 """
 Test of simple_prime_checker.PrimeChecker.is_prime_1(n) for small-ish input.
 """
+import time
+
 from simple_prime_checker import PrimeChecker
 
 
@@ -9,10 +11,40 @@ if __name__ == "__main__":
 
     prime_checker = PrimeChecker()
 
-    print(prime_checker.is_prime_1(prime_checker.prime_inputs[4]))
+    start = time.perf_counter()
+    prime_n = 25
+    result = prime_checker.is_prime_1(prime_n)
+    duration = time.perf_counter() - start
+    print(
+        "is_prime_1({0}) - duration: {1:.3f}(s)".\
+            format(prime_n, duration)
+    )
 
-    print(prime_checker.is_prime_1(25))
+    start = time.perf_counter()
+    prime_n = 25
+    result = prime_checker.is_prime_2(prime_n)
+    duration = time.perf_counter() - start
+    print(
+        "is_prime_2({0}) - duration: {1:.3f}(s)".\
+            format(prime_n, duration)
+    )
 
-    print(prime_checker.is_prime_2(prime_checker.prime_inputs[4]))
+    print('\n')
 
-    print(prime_checker.is_prime_2(25))
+    start = time.perf_counter()
+    prime_n = prime_checker.prime_inputs[4]
+    result = prime_checker.is_prime_1(prime_n)
+    duration = time.perf_counter() - start
+    print(
+        "is_prime_1({0}) - duration: {1:.3f}(s)".\
+            format(prime_n, duration)
+    )
+
+    start = time.perf_counter()
+    prime_n = prime_checker.prime_inputs[4]
+    result = prime_checker.is_prime_2(prime_n)
+    duration = time.perf_counter() - start
+    print(
+        "is_prime_2({0}) - duration: {1:.3f}(s)".\
+            format(prime_n, duration)
+    )
